a11b1b75ff01ad2343781092ec64f38fa089a630
G.W. van der Vegt <wvd_vegt@180950.no-reply.drupal.org>
2015-10-20 14:58:53 +0200

    1) Started adding files & projects.

---
+++ asset-proof-of-concept-Xamarin/asset_proof_of_concept_Xamarin/App.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.Forms;
+
+namespace asset_proof_of_concept_Xamarin
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            XAlign = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+
+            Poc poc = new Poc();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ asset-proof-of-concept-Xamarin/asset_proof_of_concept_Xamarin/Bridge.cs
@@ -0,0 +1,158 @@
+﻿// <copyright file="Bridge.cs" company="RAGE"> Copyright (c) 2015 RAGE. All rights reserved.
+// </copyright>
+// <author>Veg</author>
+// <date>13-4-2015</date>
+// <summary>Implements a Bridge with 3 interfaces</summary>
+namespace AssetPackage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using Xamarin.Forms;
+
+    /// <summary>
+    /// A bridge.
+    /// </summary>
+    class Bridge : IBridge, ILogger, IDataStorage, IDataArchive
+    {
+        /// <summary>
+        /// Initializes a new instance of the AssetPackage.Bridge class.
+        /// </summary>
+        public Bridge()
+        {
+            this.Prefix = "";
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the AssetPackage.Bridge class.
+        /// </summary>
+        ///
+        /// <param name="prefix"> The prefix. </param>
+        public Bridge(String prefix)
+            : base()
+        {
+            this.Prefix = prefix;
+        }
+
+        #region ILogger Members
+
+        /// <summary>
+        /// Executes the log operation.
+        /// 
+        /// Implement this in Game Engine Code.
+        /// </summary>
+        ///
+        /// <param name="msg"> The message. </param>
+        public void doLog(string msg)
+        {
+            //! Microsoft .Net Specific Code.
+            // 
+            Debug.WriteLine(Prefix + msg);
+        }
+
+        #endregion
+
+        #region ILogger Properties
+
+        /// <summary>
+        /// The prefix.
+        /// </summary>
+        public String Prefix
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region IDataStorage Members
+
+        IDataStorage dataStorage = DependencyService.Get<IDataStorage>();
+        IDataArchive dataArchive = DependencyService.Get<IDataArchive>();
+
+        /// <summary>
+        /// Exists the given file.
+        /// </summary>
+        ///
+        /// <param name="fileId"> The file identifier to delete. </param>
+        ///
+        /// <returns>
+        /// true if it succeeds, false if it fails.
+        /// </returns>
+        public bool Exists(string fileId)
+        {
+            return dataStorage.Exists(fileId);
+        }
+
+        /// <summary>
+        /// Gets the files.
+        /// </summary>
+        ///
+        /// <returns>
+        /// A List&lt;String&gt;
+        /// </returns>
+        public List<String> Files()
+        {
+            return dataStorage.Files();
+        }
+
+        /// <summary>
+        /// Saves the given file.
+        /// </summary>
+        ///
+        /// <param name="fileId">   The file identifier to delete. </param>
+        /// <param name="fileData"> Information describing the file. </param>
+        public void Save(string fileId, string fileData)
+        {
+            dataStorage.Save(fileId, fileData);
+        }
+
+        /// <summary>
+        /// Loads the given file.
+        /// </summary>
+        ///
+        /// <param name="fileId"> The file identifier to delete. </param>
+        ///
+        /// <returns>
+        /// A String.
+        /// </returns>
+        public string Load(string fileId)
+        {
+            return dataStorage.Load(fileId);
+        }
+
+        /// <summary>
+        /// Deletes the given fileId.
+        /// </summary>
+        ///
+        /// <param name="fileId"> The file identifier to delete. </param>
+        ///
+        /// <returns>
+        /// true if it succeeds, false if it fails.
+        /// </returns>
+        public bool Delete(string fileId)
+        {
+            return dataStorage.Delete(fileId);
+        }
+
+        #endregion
+
+        #region IDataArchive Members
+
+        /// <summary>
+        /// Archives the given file.
+        /// </summary>
+        ///
+        /// <param name="fileId"> The file identifier to delete. </param>
+        ///
+        /// <returns>
+        /// true if it succeeds, false if it fails.
+        /// </returns>
+        public bool Archive(string fileId)
+        {
+            return dataArchive.Archive(fileId);
+        }
+
+        #endregion
+    }
+}
